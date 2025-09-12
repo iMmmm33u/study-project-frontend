783c245ff1cee9c4101c4eb9f48b08491ba49e11
wcx <wcx202407@163.com>
2025-09-12 13:15:58 +0800

    完成登录页面功能以及跳转首页

---
+++ study-project-frontend/src/components/welcome/LoginPage.vue
@@ -0,0 +1,81 @@
+<template>
+    <div style="text-align: center; margin: 0 20px;">
+        <div style="margin-top: 150px;">
+            <div style="font-size: 25px; font-weight: bold;">登录</div>
+            <div style="font-size: 14px; color: grey;">在进入系统之前请先输入用户名和密码进行登录</div>
+        </div>
+        <div style="margin-top: 50px;">
+            <el-input v-model="form.username" type="text" placeholder="用户名/邮箱">
+                <template #prefix>
+                    <el-icon>
+                        <User />
+                    </el-icon>
+                </template>
+            </el-input>
+            <el-input v-model="form.password" type="password" style="margin-top: 10px;" placeholder="密码">
+                <template #prefix>
+                    <el-icon>
+                        <Lock />
+                    </el-icon>
+                </template>
+            </el-input>
+        </div>
+        <el-row style="margin-top: 5px;">
+            <el-col :span="12" style="text-align: left;">
+                <el-checkbox v-model="form.remember" label="记住我" />
+            </el-col>
+            <el-col :span="12" style="text-align: right;">
+                <el-link>忘记密码？</el-link>
+            </el-col>
+        </el-row>
+        <div style="margin-top: 40px;">
+            <el-button @click="login" style="width: 270px;" type="success" plain>立即登录</el-button>
+        </div>
+        <el-divider>
+            <span style="color: grey; font-size: 13px;">没有账号</span>
+        </el-divider>
+        <div>
+            <el-button style="width: 270px;" type="warning" plain>注册账号</el-button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { post } from "@/net"
+import { Lock, User } from "@element-plus/icons-vue"
+import { ElMessage } from "element-plus"
+import { reactive } from "vue"
+import { useRouter } from "vue-router"
+
+const router = useRouter()
+
+const form = reactive({
+    username: '',
+    password: '',
+    remember: false
+})
+
+const login = () => {
+    if (!form.username || !form.password) {
+        ElMessage.warning("请填写用户名和密码")
+    } else {
+        post('/api/auth/login', {
+            username: form.username,
+            password: form.password,
+            remember: form.remember
+        }, (message) => {
+            ElMessage.success(message)
+            console.log('登录成功，准备跳转到 /index')
+            router.push('/index').then(() => {
+                console.log('路由跳转成功')
+            }).catch(err => {
+                console.error('路由跳转失败:', err)
+            })
+        })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ study-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div style="width: 100vw; height: 100vh; overflow: hidden; display: flex">
+        <div style="flex: 1; background-color: dodgerblue;">
+            <el-image style="width: 100%; height: 100%;" fit="cover"
+                src="https://512pixels.net/wp-content/uploads/2025/08/26-Tahoe-Beach-Dawn-thumb.jpeg" />
+        </div>
+        <div class="welcome-title">
+            <div style="font-size: 30px; font-weight: bold;">欢迎来到我们的学习平台</div>
+            <div style="margin-top: 10px;">在这里你可以学习如何使用Java，如何搭建网站，并且与Java之父密切交流</div>
+        </div>
+        <div style="width: 400px; background-color: white;">
+            <router-view/>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+    .welcome-title {
+        position: absolute;
+        bottom: 30px;
+        left: 30px;
+        color: white;
+        text-shadow: 0 0 10px black;
+    }
+</style>
+
