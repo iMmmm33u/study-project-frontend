c3fd2eae4b71cf08ad0dd482bcccaa85714dd949
wcx <wcx202407@163.com>
2025-09-12 19:46:26 +0800

    完成校验功能

--- study-project-frontend/src/views/WelcomeView.vue
+++ study-project-frontend/src/views/WelcomeView.vue
@@ -8,10 +8,12 @@
             <div style="font-size: 30px; font-weight: bold;">欢迎来到我们的学习平台</div>
             <div style="margin-top: 10px;">在这里你可以学习如何使用Java，如何搭建网站，并且与Java之父密切交流</div>
         </div>
-        <div style="width: 400px; background-color: white;">
-            <transition name="el-fade-in-linear">
-                <router-view/>
-            </transition>
+        <div style="width: 400px; background-color: white; z-index: 1;">
+            <router-view v-slot="{ Component }">
+                <transition name="el-fade-in-linear">
+                    <component :is="Component" />
+                </transition>
+            </router-view>
         </div>
     </div>
 </template>
